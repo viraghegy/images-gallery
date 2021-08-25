320635bc40f1a10df257d69d909ab07e968c53a7
viraghegy <viraghegy@gmail.com>
2021-08-25 12:35:00 +0200

    controlled component

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-const Search = () => {
+const Search = ({ word, setWord, handleSubmit }) => {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <Form>
-            <Row>
-              <Col>
-                <Form.Control placeholder="Search for new images..." />
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8} lg={6}>
+          <Form onSubmit={handleSubmit}>
+            <Row>  
+              <Col xs={9}>
+                <Form.Control 
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  placeholder="Search for new images..."
+                />
               </Col>
               <Col>
                 <Button variant="primary" type="submit">Search</Button>
